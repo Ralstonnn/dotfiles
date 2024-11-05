c7671fad9ed951ad628ba457166f864b2c46bad1
ralstonnn <zigthehedge2002@gmail.com>
2024-11-06 00:19:42 +0500

    Updated fzf init script

--- .config/zsh/programs/fzf.sh
+++ .config/zsh/programs/fzf.sh
@@ -1,8 +1,8 @@
 if [ -x "$(command -v fzf)" ]; then
     source <(fzf --zsh)
     export FZF_CTRL_R_OPTS="--height 50% --layout=reverse --tmux center"
-else
-    [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+elif [ -f ~/.fzf.zsh ]; then
+    source ~/.fzf.zsh
     export FZF_CTRL_R_OPTS="--height 50% --layout=reverse --tmux center"
 fi
 
