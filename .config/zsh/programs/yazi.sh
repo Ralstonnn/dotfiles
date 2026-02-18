162b210e7d190d7bbec669701db8461ac376be8c
ralstonnn <zigthehedge2002@gmail.com>
2026-02-18 14:53:40 +0500

    added config for yazi and changed starship config not to truncate to git repo root

---
+++ .config/zsh/programs/yazi.sh
@@ -0,0 +1,10 @@
+if [ -x "$(command -v yazi)" ]; then
+    function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        command yazi "$@" --cwd-file="$tmp"
+        IFS= read -r -d '' cwd < "$tmp"
+        [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+        rm -f -- "$tmp"
+    }
+fi
+
