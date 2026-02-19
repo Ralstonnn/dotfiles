fec9596e4520f4d5c859920ed25fe83519b4f98f
ralstonnn <zigthehedge2002@gmail.com>
2026-02-20 04:18:40 +0500

    commented carapace to try zsh plugin

--- .config/zsh/programs/carapace.sh
+++ .config/zsh/programs/carapace.sh
@@ -1,5 +1,5 @@
 if [ -x "$(command -v carapace)" ]; then
-    export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
-    zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
-    source <(carapace _carapace)
+    # export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
+    # zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+    # source <(carapace _carapace)
 fi
