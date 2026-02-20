4838f4b5dce4597363d8ab921571612be7c70896
ralstonnn <zigthehedge2002@gmail.com>
2026-02-20 21:55:25 +0500

    changes to hyprland config

---
+++ installScripts/arch/install_hyprland.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+if [ ! -x "$(command -v yay)" ]; then
+    ~/dotfiles/packageInstallScripts/arch/install_yay.sh
+fi
+
+yay -S ghostty tofi dolphin google-chrome hyprland hyprpaper hypridle hyprlock hyprshutdown hyprland-guiutils hyprpolkitagent waybar eww --needed
--- packageInstallScripts/arch/install_hyprland.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-if [ ! -x "$(command -v yay)" ]; then
-    ~/dotfiles/packageInstallScripts/arch/install_yay.sh
-fi
-
-yay -S ghostty tofi dolphin google-chrome hyprland hyprpaper hypridle hyprlock waybar eww --needed
