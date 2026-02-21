6763f7915c76527ce9d404c1d11adb7b4a4611aa
ralstonnn <zigthehedge2002@gmail.com>
2026-02-21 17:11:06 +0500

    updated install plugins

--- .config/tmux/init_plugins.sh
+++ .config/tmux/init_plugins.sh
@@ -4,9 +4,6 @@ TMUX_PLUGINS_DIR=$HOME/.config/tmux/plugins
 
 if [[ ! -d $TMUX_PLUGINS_DIR ]]; then
 	mkdir $TMUX_PLUGINS_DIR 
-fi
-
-if [[ ! -d $TMUX_PLUGINS_DIR/tpm ]]; then
 	git clone https://github.com/tmux-plugins/tpm $TMUX_PLUGINS_DIR/tpm
 fi
 
