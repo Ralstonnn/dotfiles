9986c182b2e0aa48bb2f92d8c28f692660417e14
ralstonnn <zigthehedge2002@gmail.com>
2026-03-01 19:04:45 +0500

    added google and ghostty to fedora base installation

--- installScripts/fedora/base_install.sh
+++ installScripts/fedora/base_install.sh
@@ -17,14 +17,21 @@ fi
 # Install starship #
 ####################
 if ! dnf repo list --enabled | grep starship; then
-    sudo dnf copr enable atim/starship
+    sudo dnf copr enable atim/starship -y
 fi
 
 #######################
 # Enable lazygit repo #
 #######################
 if ! dnf repo list --enabled | grep lazygit; then
-    sudo dnf copr enable dejan/lazygit
+    sudo dnf copr enable dejan/lazygit -y
+fi
+
+#######################
+# Enable ghostty repo #
+#######################
+if ! dnf repo list --enabled | grep ghostty; then
+    sudo dnf copr enable scottames/ghostty -y
 fi
 
 ########################
@@ -36,5 +43,9 @@ source $BASE_SCRIPT_FOLDER/terminal_apps/install_nvm.sh
 # Install packages #
 ####################
 sudo dnf update -y && sudo dnf upgrade -y
+sudo dnf install fedora-workstation-repositories -y # Enable workstation repositories
+sudo dnf update -y && sudo dnf upgrade -y
+sudo dnf config-manager setopt google-chrome.enabled=1 -y # Enable google chrome repo
 sudo dnf install -y @development-tools clear git vim neovim tmux zsh\
-	carapace-bin zoxide fzf lazygit ripgrep stow starship unzip
+    carapace-bin zoxide fzf lazygit ripgrep stow starship unzip google-chrome-stable\
+    ghostty
