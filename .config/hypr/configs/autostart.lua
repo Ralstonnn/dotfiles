a9a6eb3a28bf7fafe924d116ef518365c8716596
ralstonnn <zigthehedge2002@gmail.com>
2026-08-05 12:30:28 +0500

    fix hyprpaper

--- .config/hypr/configs/autostart.lua
+++ .config/hypr/configs/autostart.lua
@@ -6,7 +6,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("hyprpaper")
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("systemctl --user start hyprpolkitagent")
-	hl.exec_cmd("/usr/bin/hyprland-per-window-layout")
+	-- hl.exec_cmd("/usr/bin/hyprland-per-window-layout")
 
 	hl.exec_cmd("nm-applet &")
 	hl.exec_cmd("waybar")
