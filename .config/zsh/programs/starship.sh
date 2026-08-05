38024ee99de55b4e7779c2a9a135edd0c2733fc4
ralstonnn <zigthehedge2002@gmail.com>
2026-08-05 18:55:45 +0500

    added nix function to fix starship

--- .config/zsh/programs/starship.sh
+++ .config/zsh/programs/starship.sh
@@ -1,4 +1,13 @@
 if [ -x "$(command -v starship)" ]; then
     export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
     eval "$(starship init zsh)"
+
+    nix() {
+        # NOTE: Tmp function to fix `nix shell` detection in starship
+        if [[ $1 == "shell" ]]; then
+            IN_NIX_SHELL=impure command nix "$@"
+        else
+            command nix "$@"
+        fi
+    }
 fi
