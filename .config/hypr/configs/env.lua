b3ee9c03cf05c9eda8e583b7634d65dbbad03013
ralstonnn <zigthehedge2002@gmail.com>
2026-08-05 18:55:25 +0500

    changed cursor

--- .config/hypr/configs/env.lua
+++ .config/hypr/configs/env.lua
@@ -1,8 +1,5 @@
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-
 hl.env("GDK_SCALE", "1")
 hl.env("GDK_DPI_SCALE", "1")
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
@@ -10,3 +7,5 @@ hl.env("QT_SCALE_FACTOR_ROUNDING_POLICY", "RoundPreferFloor")
 
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("HYPRCURSOR_SIZE", "18")
